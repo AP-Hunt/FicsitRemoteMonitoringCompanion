69202807c002ee680f9f82cae52c8723f65b4659
Andy Hunt <github@andyhunt.me>
2021-05-22 16:04:29 +0100

    Start Grafana when the Companion application starts

---
+++ Companion/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Companion
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            GrafanaHost.Start();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+
+            GrafanaHost.Stop();
+        }
+    }
+}
---
+++ Companion/GrafanaHost.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace Companion
+{
+    static class GrafanaHost
+    {
+        static Process _grafanaProcess;
+
+        internal static void Start()
+        {
+            string currentExeLocation = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            string currentExeDir = Path.GetDirectoryName(currentExeLocation);
+
+            string grafanaWorkingDir = Path.Combine(currentExeDir, "grafana", "bin");
+            string grafanaExePath = Path.Combine(grafanaWorkingDir, "grafana-server.exe");
+
+            string grafanaConfigPath = WriteGrafanaConfig(grafanaWorkingDir);
+
+            _grafanaProcess = Process.Start(new ProcessStartInfo()
+            {
+                FileName = grafanaExePath,
+                RedirectStandardOutput = false,
+                RedirectStandardError = false,
+                WorkingDirectory = grafanaWorkingDir,
+                UseShellExecute = true,
+
+                Arguments = $"-config \"{grafanaConfigPath}\""
+            });
+        }
+
+        private static string WriteGrafanaConfig(string grafanaWorkingDir)
+        {
+            string configPath = Path.Combine(grafanaWorkingDir, "config.ini");
+            File.WriteAllText(configPath, ConfigFileResources.GranaConfiguration);
+            return configPath;
+        }
+
+        internal static void Stop()
+        {
+            if(_grafanaProcess != null && !_grafanaProcess.HasExited)
+            {
+                _grafanaProcess.Kill();
+            }
+        }
+    }
+}
