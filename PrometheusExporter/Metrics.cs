55c37ced139a0aefc10061b86b016975768da3a6
Andy Hunt <github@andyhunt.me>
2021-05-22 14:02:01 +0100

    Implement Prometheus exporter for Ficsit Remote Monitoring mod
    See https://ficsit.app/mod/B9bEiZFtaaQZHU for Satisfactory Mod.
    
    Exporter requests the metrics coming from the mod every 5 seconds, and
    transforms them to be output in Prometheus exposition format.
    
    Also exposes a web server on port 9000 for metrics to be scraped.

---
+++ PrometheusExporter/Metrics.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrometheusExporter
+{
+    internal class Metrics
+    {
+        internal static readonly Prometheus.Gauge PowerCapacity = Prometheus.Metrics.CreateGauge(
+            "power_capacity_mw", "Total capacity of all generators in a circuit",
+            new Prometheus.GaugeConfiguration()
+            {
+                LabelNames = new string[] {
+                        "circuit_id"
+                },
+            });
+
+        internal static readonly Prometheus.Gauge PowerConsumed = Prometheus.Metrics.CreateGauge(
+            "power_consumed_mw", "Amount of power being consumed in a circuit",
+            new Prometheus.GaugeConfiguration()
+            {
+                LabelNames = new string[] {
+                        "circuit_id"
+                },
+            });
+
+        internal static readonly Prometheus.Gauge PowerMaxConsumed = Prometheus.Metrics.CreateGauge(
+            "power_max_consumed_mw", "Highest amount of power consumed in a circuit",
+            new Prometheus.GaugeConfiguration()
+            {
+                LabelNames = new string[] {
+                        "circuit_id"
+                },
+            });
+
+        internal static readonly Prometheus.Gauge PowerProduced = Prometheus.Metrics.CreateGauge(
+            "power_produced_mw", "Amount of power being produced in a circuit",
+            new Prometheus.GaugeConfiguration()
+            {
+                LabelNames = new string[] {
+                        "circuit_id"
+                },
+            });
+
+        internal static readonly Prometheus.Gauge PowerBatteryDifferential = Prometheus.Metrics.CreateGauge(
+             "power_battery_differential_pc", "Amount of battery capacity as a percentage of total capacity in a circuit",
+             new Prometheus.GaugeConfiguration()
+             {
+                 LabelNames = new string[] {
+                        "circuit_id"
+                 },
+             });
+
+        internal static readonly Prometheus.Gauge PowerBatteryPercent = Prometheus.Metrics.CreateGauge(
+             "power_battery_pc", "Amount of battery power available as a percentage of total battery capacity in a circuit",
+             new Prometheus.GaugeConfiguration()
+             {
+                 LabelNames = new string[] {
+                        "circuit_id"
+                 },
+             });
+
+        internal static readonly Prometheus.Gauge PowerBatteryCapacity = Prometheus.Metrics.CreateGauge(
+             "power_battery_capacity_mw", "Total battery capacity in a circuit",
+             new Prometheus.GaugeConfiguration()
+             {
+                 LabelNames = new string[] {
+                        "circuit_id"
+                 },
+             });
+
+        internal static readonly Prometheus.Gauge PowerBatteryTimeEmpty = Prometheus.Metrics.CreateGauge(
+             "power_battery_time_empty_seconds", "Amount of time batteries have been empty in a circuit",
+             new Prometheus.GaugeConfiguration()
+             {
+                 LabelNames = new string[] {
+                        "circuit_id"
+                 },
+             });
+
+        internal static readonly Prometheus.Gauge PowerFuseBlown = Prometheus.Metrics.CreateGauge(
+             "power_fuse_blown", "Whether the fuse has been blown in a circuit",
+             new Prometheus.GaugeConfiguration()
+             {
+                 LabelNames = new string[] {
+                        "circuit_id"
+                 },
+             });
+    }
+}
---
+++ PrometheusExporter/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Timers;
+
+namespace PrometheusExporter
+{
+    public class PowerData 
+    {
+        public int CircuitID { get; set; }
+        public double PowerCapacity { get; set; }
+        public double PowerConsumed { get; set; }
+        [System.Text.Json.Serialization.JsonPropertyName("PowerProductiuon")]
+        public double PowerProduced { get; set; }
+        public double PowerMaxConsumed { get; set; }
+        public double BatteryDifferential { get; set; }
+        public string BatteryPercent { get; set; }
+        public double BatteryCapacity { get; set; }
+        public string BatteryTimeEmpty { get; set; }
+        public bool FuseBlown { get; set; }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Prometheus.Metrics.SuppressDefaultMetrics();
+                var promServer = new Prometheus.MetricServer(hostname: "localhost", port: 9000);
+                promServer.Start();
+
+                Console.WriteLine("Begun exposing metrics at localhost:9000");
+
+                BeginMonitoringPower();
+                Console.ReadLine();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void BeginMonitoringPower() 
+        {
+            string powerUrl = "http://localhost:8090/getPowerData";
+
+            double second = 1000;
+            System.Timers.Timer timer = new System.Timers.Timer(5 * second);
+            timer.Elapsed += ReadPowerMetrics(powerUrl);
+            timer.AutoReset = true;
+            timer.Enabled = true;
+
+        }
+
+        private static ElapsedEventHandler ReadPowerMetrics(string powerUrl)
+        {
+            return (Object source, ElapsedEventArgs e) =>
+            {
+                try
+                {
+                    WebRequest req = WebRequest.Create(powerUrl);
+                    WebResponse resp = req.GetResponse();
+                    Stream responseStream = resp.GetResponseStream();
+                    StreamReader rdr = new StreamReader(responseStream);
+                    string responseJson = rdr.ReadToEnd();
+                    resp.Close(); 
+
+                    var options = new System.Text.Json.JsonSerializerOptions
+                    {
+                        AllowTrailingCommas = true,
+                        PropertyNameCaseInsensitive = true
+                    };
+
+                    List<PowerData> powerCircuits = System.Text.Json.JsonSerializer.Deserialize<List<PowerData>>(responseJson, options);
+
+                    foreach (PowerData circuit in powerCircuits)
+                    {
+                        UpdateCircuitMetrics(circuit);
+                        
+                    }
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            };
+        }
+
+        private static void UpdateCircuitMetrics(PowerData circuit)
+        {
+            string circuitId = circuit.CircuitID.ToString();
+            Metrics.PowerBatteryCapacity.WithLabels(circuitId).Set(circuit.BatteryCapacity);
+            Metrics.PowerBatteryDifferential.WithLabels(circuitId).Set(circuit.BatteryDifferential);
+            Metrics.PowerCapacity.WithLabels(circuitId).Set(circuit.PowerCapacity);
+            Metrics.PowerConsumed.WithLabels(circuitId).Set(circuit.PowerConsumed);
+            Metrics.PowerFuseBlown.WithLabels(circuitId).Set(circuit.FuseBlown? 1d : 0d);
+            Metrics.PowerMaxConsumed.WithLabels(circuitId).Set(circuit.PowerMaxConsumed);
+            Metrics.PowerProduced.WithLabels(circuitId).Set(circuit.PowerProduced);
+
+            UpdateBatteryPercentMetric(circuit);
+            UpdateBatteryTimeEmptyMetric(circuit);
+        }
+
+        private static void UpdateBatteryPercentMetric(PowerData circuit)
+        {
+            // Battery percent can come through as the string "NaN" OR the double value with a percentage sign
+            // Need to cast it all to lower case, so that "nan" isn't seen as Not A Number, which is a valid
+            // Double type value
+            double batteryPct = 0;
+            string batteryPctValue = circuit.BatteryPercent.ToLower().Trim(new Char[] { '%' });
+            bool success = double.TryParse(batteryPctValue, out batteryPct);
+
+            if (success && !double.IsNaN(batteryPct))
+            {
+                Metrics.PowerBatteryPercent.WithLabels(circuit.CircuitID.ToString()).Set(batteryPct);
+            }
+            else
+            {
+                Metrics.PowerBatteryPercent.WithLabels(circuit.CircuitID.ToString()).Set(0d);
+            }
+        }
+
+        private static void UpdateBatteryTimeEmptyMetric(PowerData circuit)
+        {
+            // Battery time empty comes through as a time span string
+            // eh hh:mm:ss
+            TimeSpan ts = TimeSpan.Zero;
+            bool success = TimeSpan.TryParse(circuit.BatteryTimeEmpty, out ts);
+
+            if(success) {
+                Metrics.PowerBatteryTimeEmpty.WithLabels(circuit.CircuitID.ToString()).Set(ts.TotalSeconds);
+            } else {
+                Metrics.PowerBatteryTimeEmpty.WithLabels(circuit.CircuitID.ToString()).Set(0);
+            }
+        }
+    }
+}
+
