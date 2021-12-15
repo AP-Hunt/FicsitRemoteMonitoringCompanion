dd6c89dd83fca68b85990b547c2b163d6ce94f3e
Andy Hunt <git@andyhunt.me>
2021-12-15 20:40:32 +0000

    Update Proxy-FactoryData to convert to GeoJSON

--- map/Proxy-FactoryData.ps1
+++ map/Proxy-FactoryData.ps1
@@ -1,4 +1,10 @@
 while(1){ 
-    wget -OutFile factory.json http://localhost:8090/getFactory; 
-    Start-Sleep -Seconds 20; 
+    wget -OutFile factory.json http://localhost:8090/getFactory
+    
+    jq -rc `
+    '{"type": \"FeatureCollection\", "features": [.[] | { "type": \"Feature\", "geometry": { "type": \"Point\", "coordinates": [.location.x, .location.y, .location.z]}, "properties": (. | del(.location))}]}' `
+    factory.json `
+    | Out-File -FilePath .\factory-geojson.json -Encoding utf8; 
+    
+    Start-Sleep -Seconds 60; 
 }
