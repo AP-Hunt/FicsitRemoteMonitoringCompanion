ee41bf876b07a900a338a58244ff0d214c9fae3b
Andy Hunt <git@andyhunt.me>
2022-01-08 15:42:23 +0000

    Implement a very basic stub for the FRM API
    It's useful to be able to run the map without having the game running.
    It doesn't matter terribly what the data is, so factory.json contains a
    dump of a small save I have.

---
+++ map/stubs/frm/index.js
@@ -0,0 +1,27 @@
+var http = require("http");
+var factoryJson = require("./factory.json");
+
+http.createServer((req, res) => {
+    switch(req.url) {
+        case "/getFactory":
+            getFactoryHandler(req, res);
+            break;
+
+        default:
+            defaultRoute(req, res);
+            break;
+    }
+}).listen(8090, () => {
+    console.log("FRM stub is listening on 8090");
+});
+
+function getFactoryHandler(req, res) {
+    res.writeHead(200, { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*"});
+    res.write(JSON.stringify(factoryJson));
+    res.end();
+}
+
+function defaultRoute(req, res) {
+    res.writeHead(404, { "Access-Control-Allow-Origin": "*"});
+    res.end();
+}
