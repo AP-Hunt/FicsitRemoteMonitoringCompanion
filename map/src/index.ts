1908f045413a1c9d735b46356dae8c31a94bde2d
Andy Hunt <git@andyhunt.me>
2022-01-08 15:40:15 +0000

    Add basic ability to query Prometheus
    Currently does a simple query and writes out the results at startup.

--- map/src/index.ts
+++ map/src/index.ts
@@ -1,8 +1,29 @@
 import { GameMap } from './map';
 
+/// <reference types="prometheus-query/dist/types" />
+
 function init()
 {
     let map = new GameMap(document.getElementById("map")!);
     map.plotBuildings("http://localhost:8090/getFactory");
+
+    // @ts-ignore TS2304
+    const prom = new Prometheus.PrometheusDriver({
+        endpoint: "http://localhost:9090",
+        baseURL: "/api/v1"
+    });
+
+    prom.rangeQuery(
+        'machine_items_produced_per_min{item_name="Iron Ingot"} > 20',
+        new Date("2022-01-06T19:40:00Z"),
+        new Date("2022-01-06T19:44:00Z"),
+        60,
+    ).then((res : any) => {
+        const series = res.result;
+        series.forEach((serie:any) => {
+            console.log("Serie:", serie.metric.toString());
+            console.log("Values:\n" + serie.values.join('\n'));
+        });
+    });
 }
 init();
