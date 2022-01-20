01432fa333be5019889e4380d84df67cffa6aaed
Andy Hunt <git@andyhunt.me>
2022-01-20 20:42:14 +0000

    Coordinate Prometheus and exporter processes
    Implements a simple text-based UI which is shown after the two processes
    are started.
    
    Ctrl+C exits the program, which shuts down the other processes before
    stopping.

--- Companion/main.go
+++ Companion/main.go
@@ -1,45 +1,61 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
+	"os/signal"
 
 	"github.com/AP-Hunt/FicsitRemoteMonitoringCompanion/m/v2/exporter"
 	"github.com/AP-Hunt/FicsitRemoteMonitoringCompanion/m/v2/prometheus"
 )
 
 func main() {
-	prom, err := prometheus.NewPrometheusWrapper()
+	// Create exporter
+	promExporter := exporter.NewPrometheusExporter("http://localhost:8090")
 
+	// Create prometheus
+	prom, err := prometheus.NewPrometheusWrapper()
 	if err != nil {
-		fmt.Printf("error starting prometheus: %s", err)
+		fmt.Printf("error preparing prometheus: %s", err)
 		os.Exit(1)
 	}
 
+	// Start prometheus
 	err = prom.Start()
 	if err != nil {
 		fmt.Printf("error starting prometheus: %s", err)
 		os.Exit(1)
 	}
 
-	promExporter := exporter.NewPrometheusExporter("http://localhost:8090")
+	// Start exporter
 	promExporter.Start()
 
-	fmt.Println("Processes are running. Press enter to stop.")
-	rdr := bufio.NewReader(os.Stdin)
-	_, _ = rdr.ReadString('\n')
-	fmt.Println("Exiting.")
+	fmt.Print(`
+Ficsit Remote Monitoring Companion
 
+To access metrics in Prometheus visit:
+http://localhost:9090/
+
+Press Ctrl + C to exit.
+`)
+
+	// Wait for an interrupt signal
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt)
+	<-sigChan
+
+	// Stop the exporter
 	err = promExporter.Stop()
 	if err != nil {
 		fmt.Printf("error stopping prometheus exporter: %s", err)
 	}
 
+	// Stop prometheus
 	err = prom.Stop()
 	if err != nil {
 		fmt.Printf("error stopping prometheus: %s", err)
 	}
 
+	fmt.Println("Exiting.")
 	os.Exit(0)
 }
