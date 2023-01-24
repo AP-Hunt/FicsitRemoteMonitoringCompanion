3f0402d9e45994373e5a0d4bb43d45f50021f7ae
Jeff Wong <awole20@gmail.com>
2023-01-23 23:04:39 -0800

    FIX: get consumption and production capacity from raw values
    Do not parse ProdPerMin values

--- Companion/exporter/production_collector.go
+++ Companion/exporter/production_collector.go
@@ -2,78 +2,20 @@ package exporter
 
 import (
 	"log"
-	"regexp"
-	"strconv"
 )
 
 type ProductionCollector struct {
 	FRMAddress string
 }
 
-var prodPerMinRegex = regexp.MustCompile(`P: (?P<prod_current>[\d.]+)/(?P<prod_capacity>[\d.]+)/min - C: (?P<cons_current>[\d.]+)/(?P<cons_capacity>[\d.]+)/min`)
-
 type ProductionDetails struct {
 	ItemName           string   `json:"ItemName"`
-	ProdPerMin         string   `json:"ProdPerMin"`
-	ProdPercent        *float64 `json:"ProdPercent"`
-	ConsPercent        *float64 `json:"ConsPercent"`
+	ProdPercent        float64 `json:"ProdPercent"`
+	ConsPercent        float64 `json:"ConsPercent"`
 	CurrentProduction  float64  `json:"CurrentProd"`
 	CurrentConsumption float64  `json:"CurrentConsumed"`
 	MaxProd            float64  `json:"MaxProd"`
 	MaxConsumed        float64  `json:"MaxConsumed"`
-}
-
-func (pd *ProductionDetails) parseProdPerMin() (bool, map[string]string) {
-	match := prodPerMinRegex.FindStringSubmatch(pd.ProdPerMin)
-
-	if len(match) < 1 {
-		return false, nil
-	}
-
-	paramsMap := make(map[string]string)
-	for i, name := range prodPerMinRegex.SubexpNames() {
-		if i > 0 && i <= len(match) {
-			paramsMap[name] = match[i]
-		}
-	}
-
-	return true, paramsMap
-}
-
-func (pd *ProductionDetails) ItemProductionCapacity() *float64 {
-	hasMatched, params := pd.parseProdPerMin()
-
-	if !hasMatched {
-		return nil
-	}
-
-	value := params["prod_capacity"]
-
-	v, err := strconv.ParseFloat(value, 64)
-
-	if err != nil {
-		return nil
-	}
-
-	return &v
-}
-
-func (pd *ProductionDetails) ItemConsumptionCapacity() *float64 {
-	hasMatched, params := pd.parseProdPerMin()
-
-	if !hasMatched {
-		return nil
-	}
-
-	value := params["cons_capacity"]
-
-	v, err := strconv.ParseFloat(value, 64)
-
-	if err != nil {
-		return nil
-	}
-
-	return &v
 }
 
 func NewProductionCollector(frmAddress string) *ProductionCollector {
@@ -94,18 +36,9 @@ func (c *ProductionCollector) Collect() {
 		ItemsProducedPerMin.WithLabelValues(d.ItemName).Set(d.CurrentProduction)
 		ItemsConsumedPerMin.WithLabelValues(d.ItemName).Set(d.CurrentConsumption)
 
-		ItemProductionCapacityPercent.WithLabelValues(d.ItemName).Set(*d.ProdPercent)
-		ItemConsumptionCapacityPercent.WithLabelValues(d.ItemName).Set(*d.ConsPercent)
-
-		prodCapacity := d.ItemProductionCapacity()
-		consCapacity := d.ItemConsumptionCapacity()
-
-		if prodCapacity != nil {
-			ItemProductionCapacityPerMinute.WithLabelValues(d.ItemName).Set(*prodCapacity)
-		}
-
-		if consCapacity != nil {
-			ItemConsumptionCapacityPerMinute.WithLabelValues(d.ItemName).Set(*consCapacity)
-		}
+		ItemProductionCapacityPercent.WithLabelValues(d.ItemName).Set(d.ProdPercent)
+		ItemConsumptionCapacityPercent.WithLabelValues(d.ItemName).Set(d.ConsPercent)
+		ItemProductionCapacityPerMinute.WithLabelValues(d.ItemName).Set(d.MaxProd)
+		ItemConsumptionCapacityPerMinute.WithLabelValues(d.ItemName).Set(d.MaxConsumed)
 	}
 }
